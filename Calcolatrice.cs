3c9d1cd2eff73bd214efc408150affb6d18fa603
DaveCorleto <davide.corleto@fastwebnet.it>
2024-04-23 14:49:24 +0200

    Somma Numeri interi

---
+++ Calcolatrice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class Calcolatrice
+    {
+        static Random Rand = new Random();
+
+        // Metodo per generare un numero intero casuale
+        public static int GeneraNumeroRandom()
+        {
+            int n = Rand.Next(1,999);
+            return n;
+        }
+
+        // Costruttore statico per inizializzare num1 con un numero casuale
+        static Calcolatrice()
+        {
+            int num1 = GeneraNumeroRandom();
+        }
+
+        // Metodo per la somma di due numeri interi
+        public static int SommaNumeriInteri(int num1, int num2)
+        {
+            int risultato = num1 + num2;
+            return risultato;
+        }
+        // Altri metodi possono essere aggiunti qui...
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1 = Calcolatrice.GeneraNumeroRandom();
+            Console.WriteLine($"Numero uno generato: {num1}");
+            int num2 = Calcolatrice.GeneraNumeroRandom();
+            Console.WriteLine($"Numero uno generato: {num2}");
+
+            Console.WriteLine("Ecco la somma di questi due interi: ");
+            Console.WriteLine(Calcolatrice.SommaNumeriInteri(num1,num2));
+
+
         }
     }
 }
