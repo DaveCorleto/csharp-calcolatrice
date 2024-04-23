89319333c70be67a257bcf98db6e8d847abd3950
DaveCorleto <davide.corleto@fastwebnet.it>
2024-04-23 15:26:54 +0200

    Moltiplicazione + Sottrazione

--- Calcolatrice.cs
+++ Calcolatrice.cs
@@ -17,10 +17,19 @@ namespace csharp_calcolatrice
             return n;
         }
 
+        // Metodo per generare un numero intero casuale
+        public static double GeneraNumeroRandomDouble()
+        {
+            double n = Rand.NextDouble();
+            n = Math.Round(n,4);
+            return n;
+        }
+
         // Costruttore statico per inizializzare num1 con un numero casuale
         static Calcolatrice()
         {
             int num1 = GeneraNumeroRandom();
+            double numdouble1 = GeneraNumeroRandomDouble();
         }
 
         // Metodo per la somma di due numeri interi
@@ -29,7 +38,50 @@ namespace csharp_calcolatrice
             int risultato = num1 + num2;
             return risultato;
         }
-        // Altri metodi possono essere aggiunti qui...
+        
+
+        //Differenza tra due numeri interi 
+
+        public static int SottraiNumeriInteri(int num1, int num2)
+        {
+            int risultato = num1 - num2;
+            return risultato;
+        }
+
+        //Moltiplicazione tra due numeri interi 
+
+        public static int MoltiplicaNumeriInteri(int num1, int num2)
+        {
+            int risultato = num1 * num2;
+            return risultato;
+        }
+
+        //Metodo per la somma di sue numeri Double 
+
+        public static double SommanumeriDouble(double numdouble1, double numdouble2)
+        {
+            double risultato = numdouble1 + numdouble2;
+            return risultato;
+        }
+
+        //Differenza tra due numeri double
+
+        public static double SottraiNumeriDouble(double numdouble1, double numdouble2)
+        {
+            double risultato = numdouble1 - numdouble2;
+            return risultato;
+        }
+
+        //Moltiplica tra due numeri double
+
+        public static double MoltiplicaNumeriDouble(double numdouble1, double numdouble2)
+        {
+            double risultato = numdouble1 * numdouble2;
+            return risultato;
+        }
+
+
+
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -3,14 +3,35 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+
+
+        {   
+
+            
             int num1 = Calcolatrice.GeneraNumeroRandom();
             Console.WriteLine($"Numero uno generato: {num1}");
             int num2 = Calcolatrice.GeneraNumeroRandom();
-            Console.WriteLine($"Numero uno generato: {num2}");
+            Console.WriteLine($"Numero due generato: {num2}");
 
-            Console.WriteLine("Ecco la somma di questi due interi: ");
+            Console.WriteLine($"Ecco la somma di questi due interi: {num1} + {num2} = ");
             Console.WriteLine(Calcolatrice.SommaNumeriInteri(num1,num2));
+
+            Console.WriteLine($"Ecco la sottrazione di questi due interi: {num1} - {num2} = ");
+            Console.WriteLine(Calcolatrice.SottraiNumeriInteri(num1, num2));
+
+            Console.WriteLine($"Ecco la moltiplicazione di questi due interi: {num1} X {num2} = ");
+            Console.WriteLine(Calcolatrice.MoltiplicaNumeriInteri(num1, num2));
+
+            double numdouble1 = Calcolatrice.GeneraNumeroRandomDouble();
+            Console.WriteLine($"Numero double uno generato: {numdouble1}");
+            double numdouble2 = Calcolatrice.GeneraNumeroRandomDouble();
+            Console.WriteLine($"Numero double due generato: {numdouble2}");
+
+            Console.WriteLine($"Ecco la somma di questi due double: {numdouble1} + {numdouble2} = "); Console.WriteLine(Calcolatrice.SommanumeriDouble(numdouble1,numdouble2));
+
+            Console.WriteLine($"Ecco la sottrazione dei due double: {numdouble1} - {numdouble2} = "); Console.WriteLine(Calcolatrice.SottraiNumeriDouble(numdouble1, numdouble2));
+
+            Console.WriteLine($"Ecco la moltiplicazione dei due double: {numdouble1} x {numdouble2} = "); Console.WriteLine(Calcolatrice.MoltiplicaNumeriDouble(numdouble1, numdouble2));
 
 
         }
